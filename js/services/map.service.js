b95393a70d4493d4926c9fde03485eb3ada99d90
Liron Shahar-Atia <liron.shaharatia@gmail.com>
2019-10-30 18:06:11 +0200

    Starting Weather

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -8,17 +8,17 @@ export default {
 var map;
 
 export function initMap(lat = 32.0749831, lng = 34.9120554) {
-    console.log('InitMap'); 
+    console.log('InitMap');
     return _connectGoogleApi()
-    .then(() => {
-        console.log('google available');
-        map = new google.maps.Map(
-            document.querySelector('#map'), {
+        .then(() => {
+            console.log('google available');
+            map = new google.maps.Map(
+                document.querySelector('#map'), {
                 center: { lat, lng },
                 zoom: 15
             })
-        console.log('Map!', map);
-    })
+            console.log('Map!', map);
+        })
 }
 
 function addMarker(loc) {
@@ -31,7 +31,7 @@ function addMarker(loc) {
 }
 
 function panTo(lat, lng) {
-    var laLatLng = new google.maps.LatLng( lat,  lng);
+    var laLatLng = new google.maps.LatLng(lat, lng);
     map.panTo(laLatLng);
 }
 
@@ -42,10 +42,26 @@ function _connectGoogleApi() {
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
     document.body.append(elGoogleApi);
-    
+
     return new Promise((resolve, reject) => {
         elGoogleApi.onload = resolve;
         elGoogleApi.onerror = () => reject('Google script failed to load')
+    })
+}
+
+
+// WEATHER API
+function _connectWeatherApi() {
+    if (window.google) return Promise.resolve() // ?
+    const W_KEY = '7cbe267230f50e0c17a4237c58492a01'; // Weather key
+    var elWheatherApi = document.createElement('script');
+    elWheatherApi.src = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${W_KEY}`;
+    elWheatherApi.async = true;
+    document.body.append(elWheatherApi);
+
+    return new Promise((resolve, reject) => {
+        elWheatherApi.onload = resolve;
+        elWheatherApi.onerror = () => reject('Weather script failed to load')
     })
 }
 
